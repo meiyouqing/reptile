14262e0a457fc6e1b5650a4b70a8ee787bf08ec1
unknown <meimingming@globale.com>
2017-05-10 12:00:41 +0800

    add article

--- vamei/save.js
+++ vamei/save.js
@@ -1,11 +1,18 @@
 // 一些依赖库
 const fs = require("fs");
 
-const save = function(obj){
+const save = function(obj, path){
+	path = path.split('-');
+	path = path.reduce((accumulator, currentValue, currentIndex, array) => {
+		if(!fs.existsSync(accumulator+currentValue)){
+			fs.mkdirSync(accumulator+currentValue);
+		}
+		return accumulator+currentValue+'/';
+	}, './data/')
+
     const js = JSON.stringify(obj)
-    const filename = Date.now();
-    fs.writeFileSync('./data/'+filename+'.json', js)
-    console.log('saved '+filename+'.json');
+    fs.writeFileSync(path+'article.json', js)
+    console.log('saved '+path+'article.json');
 }
 
 exports.start = save;
